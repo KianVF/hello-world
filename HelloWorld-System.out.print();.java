c75149d2bd70e98f23a2d135fa525ff745ab9e24
KianVF <41788899+KianVF@users.noreply.github.com>
2018-07-28 17:08:49 +0430

    Update HelloWorld-System.out.print();.java

--- HelloWorld-System.out.print();.java
+++ HelloWorld-System.out.print();.java
@@ -3,7 +3,7 @@ package hello.world;
 public class HelloWorld lllh{
 
     public static void main(String[] args) {
-        //this would print "Hello,World!" in the cosooleo
+        //this would print "Hello,World!" in the console (Without double quotations indeed!)
         System.out.print("Hello,World!");
     }
 

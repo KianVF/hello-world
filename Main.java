94fd031ed034ee2e7696c0d0db613acb14a6a982
KianVF <41788899+KianVF@users.noreply.github.com>
2018-07-28 17:23:26 +0430

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(new JFrame(), "Hello,World!");
+
+	}
+
+}
